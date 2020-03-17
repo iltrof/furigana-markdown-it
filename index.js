11c6c428ae6e5be2c276fadf8fe04d408ead6937
iltrof <trofilya@gmail.com>
2020-03-17 20:14:38 +0100

    Convert everything to camel case
    Forgot this wasn't Python.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 
-module.exports = function furigana_plugin(md, options) {
+module.exports = function furiganaPlugin(md, options) {
   md.inline.ruler.push("furigana", require("./lib/furigana"));
 };
